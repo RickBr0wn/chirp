01f38af95f065b97cb5f54b5a2105947029ffce3
Rick Brown <ricky.brown.00@gmail.com>
2023-07-08 13:51:47 +0100

    broken middleware file

--- src/pages/middleware.ts
+++ src/pages/middleware.ts
@@ -1,12 +1,13 @@
-import { withClerkMiddleware } from "@clerk/nextjs"
-import { NextRequest, NextResponse } from "next/server"
+import { withClerkMiddleware } from '@clerk/nextjs'
+import { type NextRequest, NextResponse } from 'next/server'
 
 export default withClerkMiddleware((req: NextRequest) => {
-  console.log("Middleware running")
+  console.log('Middleware running')
+  console.log(req)
   return NextResponse.next()
 })
 
 // Stop middleware from running on static files
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).)*",
+  matcher: '/((?!_next/image|_next/static|favicon.ico).)*',
 }
